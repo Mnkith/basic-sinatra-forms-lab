c4fb816d65928e471301ba75919bb6e53ba11d9b
Mnkith <munkithabid@gmail.com>
2021-01-15 01:53:02 -0600

    commit tree 73d2f679e231b0e4494315019d71493f8a642810

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:name]
+    @roles = ['Coach', 'Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center']
+    @players = params.values[1..-1]
+    erb :team
+  end
 
 end
